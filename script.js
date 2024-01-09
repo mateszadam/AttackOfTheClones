989017edba9b03310470e0aaa76641f7e3acfd03
kissmarcell132 <kiss.marcell@students.jedlik.eu>
2024-01-09 13:20:02 +0100

    marci

--- script.js
+++ script.js
@@ -3,7 +3,63 @@ let Species;
 let Planets;
 let Starships;
 
+function createDiv(thing, select) {
+    let div = document.createAttribute('div');
+    let ul = document.createAttribute('ul');
+    let ulInner = '';
+    if (select === 'species')
+        ulInner = `  
+            <li>Name: ${thing.name}</li>
+            <li>Classification: ${thing.classification}</li>
+            <li>Designation: ${thing.designation}</li>
+            <li>Average height: ${thing.average_height}</li>
+            <li>Skin colors: ${thing.skin_colors}</li>
+            <li>Hair colors: ${thing.hair_colors}</li>
+            <li>Eye colors: ${thing.eye_colors}</li>
+            <li>Average lifespan: ${thing.average_lifespan}</li>
+            <li>Homeworld: ${thing.homeworld}</li>
+            <li>Language: ${thing.language}</li>
+        `
+    else if (select == 'people')
+        ulInner = `
+            <li>Name: ${thing.name}</li>
+            <li>Height: ${thing.height}</li>
+            <li>Mass: ${thing.mass}</li>
+            <li>Hair color: ${thing.hair_color}</li>
+            <li>Skin color: ${thing.skin_color}</li>
+            <li>Eye color: ${thing.eye_color}</li>
+            <li>Birth year: ${thing.birth_year}</li>
+            <li>Gender: ${thing.gender}</li>
+        `
+    else if (select == 'planets')
+        ulInner = `
+            <li>Name: ${thing.name}</li>
+            <li>Rotation period: ${thing.rotation_period}</li>
+            <li>Orbital period: ${thing.orbital_period}</li>
+            <li>Diameter: ${thing.diameter}</li>
+            <li>Climate: ${thing.climate}</li>
+            <li>Gravity: ${thing.gravity}</li>
+            <li>Terrain: ${thing.terrain}</li>
+            <li>Population: ${thing.population}</li>
+        `
+    else if (select == 'starships'){
+        ulInner = `[{"name":"CR90 corvette","model":"CR90 corvette","manufacturer":"Corellian Engineering Corporation","cost_in_credits":"3500000","length":"150","max_atmosphering_speed":"950","crew":"30-165","passengers":"600","cargo_capacity":"3000000","consumables":"1 year","hyperdrive_rating":"2.0","MGLT":"60","starship_class":"corvette","pilots":[],"films":["1","3","6"],"created":"2014-12-10T14:20:33.369000Z","edited":"2014-12-20T21:23:49.867000Z","url":"2","id":"2"}]
+            <li>Name: ${thing.name}</li>
+            <li>Model: ${thing.model}</li>
+            <li>Manufacturer: ${thing.manufacturer}</li>
+            <li>Cost in credits: ${thing.cost_in_credits}</li>
+            <li>Length: ${thing.lenght}</li>
+            <li>Max speed: ${thing.max_atmosphering_speed}</li>
+            <li>Crew: ${thing.crew}</li>
+            <li>Passengers: ${thing.passengers}</li>
 
-function(params) {
-    
+        `
+    }
+}
+
+function selectById(e) {
+    const selected = e.dataset.id;
+    const response = fetch('https://bgs.jedlik.eu/swapi/api/' + selected);
+    const thing = response.json();
+    this.createDiv(thing, selected);
 }
